19b4d92930ee02d0e9a7feeac9dc7009382df5c0
SkootyPex <kallej@outlook.dk>
2025-11-10 16:26:02 +0100

    add lamport locks into main

--- server/node/main.go
+++ server/node/main.go
@@ -14,12 +14,14 @@ import (
 
 type server struct {
 	pb.UnimplementedMutexServer
-	id string
+	id    string
+	clock *LamportClock
 }
 
 func (s *server) RequestAccess(ctx context.Context, req *pb.Request) (*pb.Reply, error) {
+	s.clock.Receive(req.Timestamp)
 	log.Printf("%s got a request from %s Timestamp: %d", s.id, req.From, req.Timestamp)
-	return &pb.Reply{From: s.id, Ok: true}, nil
+	return &pb.Reply{From: s.id, Ok: true}, nil //always grant it for now, temp
 }
 
 func main() {
@@ -28,13 +30,15 @@ func main() {
 	peer := flag.String("peer", "", "optional address (host:port)")
 	flag.Parse()
 
+	clock := &LamportClock{}
+
 	go func() {
 		lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 		if err != nil {
 			log.Fatalf("Failed to listeon on %v", err)
 		}
 		s := grpc.NewServer()
-		pb.RegisterMutexServer(s, &server{id: *id})
+		pb.RegisterMutexServer(s, &server{id: *id, clock: clock})
 
 		log.Printf("%s Listening on: %d", *id, *port)
 		if err := s.Serve(lis); err != nil {
@@ -51,14 +55,16 @@ func main() {
 		}
 		defer conn.Close()
 		client := pb.NewMutexClient(conn)
+		clock.Tick()
+		ts := clock.Time()
 
-		log.Printf("%s is sending requestAccess to %s", *id, *peer)
+		log.Printf("%s (%d) is sending requestAccess to %s", *id, ts, *peer)
 		resp, err := client.RequestAccess(context.Background(),
-			&pb.Request{From: *id, Timestamp: time.Now().UnixNano()})
+			&pb.Request{From: *id, Timestamp: ts})
 		if err != nil {
 			log.Fatalf("error %v", err)
 		}
-		log.Printf("%s got a reply from %s, %v", *id, resp.From, resp.Ok)
+		log.Printf("%s (%d) got a reply from %s, %v", *id, clock.Time(), resp.From, resp.Ok)
 	}
 	select {}
 }
